e028ae57c986bdadd215561537ad9ecab0f93a4e
Christopher Tate <computate@computate.org>
2022-05-24 14:03:04 -0600

    Fixed handling of zoned date times.

--- src/main/java/org/computate/vertx/handlebars/DateHelpers.java
+++ src/main/java/org/computate/vertx/handlebars/DateHelpers.java
@@ -1,11 +1,15 @@
 package org.computate.vertx.handlebars;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 
+import org.apache.commons.lang3.StringUtils;
 import org.computate.search.serialize.ComputateZonedDateTimeSerializer;
 
 import com.github.jknack.handlebars.Helper;
@@ -29,7 +33,17 @@ public enum DateHelpers implements Helper<Object> {
 			String zone = options.param(2);
 			ZoneId zoneId = ZoneId.of(zone);
 			String str = (String)a;
-			ZonedDateTime d = ZonedDateTime.parse(str, ComputateZonedDateTimeSerializer.ZONED_DATE_TIME_FORMATTER);
+			ZonedDateTime d = null;
+
+			if(StringUtils.endsWith(str, "]"))
+				d = str == null ? null : ZonedDateTime.parse(str, ComputateZonedDateTimeSerializer.ZONED_DATE_TIME_FORMATTER);
+			else if(StringUtils.endsWith(str, "Z"))
+				d = str == null ? null : Instant.parse(str).atZone(zoneId).truncatedTo(ChronoUnit.MILLIS);
+			else if(StringUtils.contains(str, "T"))
+				d = str == null ? null : ZonedDateTime.parse(str, DateTimeFormatter.ISO_DATE_TIME).truncatedTo(ChronoUnit.MILLIS);
+			else
+				d = str == null ? null : LocalDate.parse(str, DateTimeFormatter.ISO_DATE).atStartOfDay(zoneId).truncatedTo(ChronoUnit.MILLIS);
+
 			return DateTimeFormatter.ofPattern(pattern, locale).format(d.withZoneSameInstant(zoneId));
 		}
 	}
