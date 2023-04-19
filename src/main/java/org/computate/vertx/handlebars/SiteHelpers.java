190496c902bac42066ddf2d9d7e3e31862e7f1b3
Christopher Tate <computate@computate.org>
2023-04-19 07:25:58 -0600

    Add a numberAdd Handlebars helper

--- src/main/java/org/computate/vertx/handlebars/SiteHelpers.java
+++ src/main/java/org/computate/vertx/handlebars/SiteHelpers.java
@@ -288,6 +288,27 @@ public enum SiteHelpers implements Helper<Object> {
 	/**
 	 * Return true if the value is an even number, otherwise false
 	 */
+	numberAdd{
+		@Override
+		public Object apply(final Object value, final Options options) throws IOException {
+			isTrue(value instanceof Number, "found '%s', expected 'number'", value);
+			Number number1 = (Number) value;
+			Object object2 = options.param(0);
+			isTrue(object2 instanceof Number, "found '%s', expected 'number'", object2);
+			Number number2 = (Number) object2;
+			if(number1 instanceof Integer && number2 instanceof Integer)
+				return (Integer)number1 + (Integer)number2;
+			else if(number1 instanceof Double && number2 instanceof Double)
+				return (Double)number1 + (Double)number2;
+			else
+				return null;
+		}
+	},
+
+
+	/**
+	 * Return true if the value is an even number, otherwise false
+	 */
 	even{
 		@Override
 		public Object apply(final Object value, final Options options) throws IOException {
